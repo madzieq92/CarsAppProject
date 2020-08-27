cad7df45ad12a27ff02205cf95f6643f08aad03c
Magdalena Peplowska <madzieq92@gmail.com>
2020-08-27 18:48:21 +0200

    Renamed project files path and disabled jdbc dependency. App built successfully.

--- src/main/java/com/example/cars/Application.java
+++ src/main/java/com/example/cars/Application.java
@@ -1,4 +1,4 @@
-package com.example.application;
+package com.example.cars;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
